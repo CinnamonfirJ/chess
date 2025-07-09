9aef1e1bcf7b0a56e703c3303c336e24bbed3ae1
CinnamonfirJ <cinnamonfir@gmail.com>
2025-07-09 15:50:26 +0100

    Added game screen

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-greeting = "Hello Kumba"
+import pygame
+from pygame import *
+import sys
+
+
+# Screen  Sizes
+WIDTH = 1080
+HEIGHT = 720
+
+game_title = "Jesse and Kumba chess game"
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(game_title)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+
+        
+pygame.init()   
